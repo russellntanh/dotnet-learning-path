ede77e35b17c18f32cc508715f405d7a34a06110
Russell Nguyen <russell.nguyen@kohyoung.com>
2026-03-12 08:24:01 +0700

    [day-02] Create project that using class and method

---
+++ phase-a-fundamentals/week-01/day-02-class-object/TeamManager/TeamManager/Developer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamManager
+{
+    public class Developer
+    {
+        public string Name;
+        public int Age;
+        public string Posision;
+        public double YearOfExperience;
+        public bool IsFullTime;
+
+        public string GetFullTimeStatus()
+        {
+            return IsFullTime ? "Yes" : "No";
+        }
+    }
+}
